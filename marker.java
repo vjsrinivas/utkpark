fd45835cf78b88843e9d999f4107be1e62358902
unknown <vishalaiely@yahoo.com>
2018-09-29 15:10:24 -0400

    new functions to get name, lat, long

--- marker.java
+++ marker.java
@@ -7,3 +7,27 @@ import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
+
+
+private String lot;
+
+private float lat;
+
+private float lng;
+
+
+public String getGarage(String lot) {
+        this.lot = lot;
+    return lot;
+}
+
+public float getLat(float lat) {
+    this.lat = lat;
+    return lat;
+}
+
+public float getLng(float lng) {
+    this.lng = lng;
+    return lng;
+}
+
