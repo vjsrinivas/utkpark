526e548f6684edf3d34d276748345dbc5c9307df
unknown <vishalaiely@yahoo.com>
2018-09-29 18:07:55 -0400

    New class for setting vars

--- marker.java
+++ marker.java
@@ -8,26 +8,78 @@ import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
+Object obj = new JSONParser().rparse(new FileReader("pakingcords.json"));
 
-private String lot;
+JSONObject parking = (JSONObject) obj;
 
-private float lat;
+JSONArray park = (JSONArray) park.get("parking");
 
-private float lng;
+class parkingMark{
+    String lot;
+    float lat;
+    float lng;
+
+    private List<String> lot_names = new ArrayList<>();
+
+    public void setGarage(String name){
+        lot = name;
+    }
+
+    public void setLat(float latIn){
+        lat = latIn;
+    }
+
+    public void setLng(float lngIn){
+        lng = lngIn;
+    }
+
+    public List<> getName(){
+
+    }
+}
+
+Iterator eachPark = park.Iterator();
+
+for(){
+
+}
 
 
-public String getGarage(String lot) {
-        this.lot = lot;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+public String getGarage(String lotIn) {
+    for(Object i : "parking"){
+        parkingMark  = new parkingMark();
+    }
     return lot;
 }
 
-public float getLat(float lat) {
-    this.lat = lat;
+public float getLat(float latIn) {
+    for(Object i : "parking"){
+        float lat = this.i.lat;
+    }
     return lat;
 }
 
 public float getLng(float lng) {
-    this.lng = lng;
+    park.lng = lng;
     return lng;
+}
+
+public void listLots() {
+
 }
 
